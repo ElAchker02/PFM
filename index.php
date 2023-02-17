85351a855af17e7d8d63ec91f97e4e7af1e06054
ELachker02 <amineelachker02@gmail.com>
2023-02-17 15:02:37 +0100

    .

--- index.php
+++ index.php
@@ -14,7 +14,8 @@
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 
 <body>
@@ -82,13 +83,69 @@
                     <button type="button" class="btn btn-danger" id="deleteTab">Supprimmer la table</button>
                 </div>
                 <div class="col-sm">
-                    <a href="addRelations.php?db=<?php echo $_GET['db']."&id=". $_GET['id'] ; ?>" class="btn btn-secondary" target="_blank" id="relations">Creer les relation</a>
+                    <a href="addRelations.php?db=<?php echo $_GET['db']."&id=". $_GET['id'] ; ?>"
+                        class="btn btn-secondary" target="_blank" id="relations">Creer les relation</a>
 
                 </div>
                 <div class="col-sm">
-                    <button type="button" class="btn btn-success" id="inserer">Inserer des enregistrement</button>
+                    <button type="button" class="btn btn-success" id="inserer" data-toggle="modal"
+                        data-target="#exampleModal">Inserer des enregistrement</button>
                 </div>
             </div>
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
+                <form action="" method="post">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <table>
+                                <?php
+				                    $sql5 = "SELECT * FROM `columns` WHERE idTable =". $_GET['id'];
+                                    $results5 =  $cnx->query($sql5);
+                                    while($row = $results5->fetch_row()){?>
+                                    <tr>
+                                            <?php 
+                                             if($row[1] == "VARCHAR" || $row[1] == "TEXT"){?>
+                                             <td><label for="<?php echo $row[0];?>"><?php echo $row[0];?></label></td>
+                                             <td><input type="text" name="<?php echo $row[0];?>" id="<?php echo $row[0];?>" class="form-control" required></td>   
+                                            <?php } 
+                                            elseif($row[1] == "DATE"){ ?>
+                                            <td><label for="<?php echo $row[0];?>"><?php echo $row[0];?></label></td>
+                                            <td><input type="date" name="<?php echo $row[0];?>" id="<?php echo $row[0];?>" class="form-control"required></td>
+                                            <?php } 
+                                            elseif ($row[1] == "INT" ){?>
+                                           <td> <label for="<?php echo $row[0];?>"><?php echo $row[0];?></label></td>
+                                           <td><input type="number" name="<?php echo $row[0];?>" id="<?php echo $row[0];?>" class="form-control" required ></td>
+                                            <?php }
+                                            elseif ( $row[1] == "FLOAT" || $row[1] == "DOUBLE" || $row[1] == "DECIMAL"){?>
+                                            <td> <label for="<?php echo $row[0];?>"><?php echo $row[0];?></label></td>
+                                            <td><input type="text" name="<?php echo $row[0];?>" id="<?php echo $row[0];?>" class="form-control" pattern = "[+-]?([0-9]*[.])?[0-9]+" required></td>
+                                                
+                                            
+                                        <?php } ?> </tr> <?php } ?>
+
+                                    
+                                    
+                                    </table>
+                                
+                            </div>
+                            <div class="modal-footer">
+                                <input type="submit" value="Sauvegarder" class="btn btn-secondary" name="btnSub">
+                                <input type="reset" value="Vider" class="btn btn-secondary" >
+                            </div>
+                        </div>
+                    </div>
+                </form>
+
+            </div>
+
             <table class="table table-sm mt-5">
                 <thead>
                     <tr>
@@ -118,7 +175,6 @@
 			}
 	  ?>
                 </tbody>
-
             </table>
             <h4>Enregistrement</h4>
 
@@ -129,33 +185,83 @@
                         $sql = "SELECT * FROM `columns` WHERE idTable =". $_GET['id'];
                         $results =  $cnx->query($sql);
                         $i = 0;
+                        $primaryIndex =-1;
+                        $columns =array();
+                        $columnsNamee = array();
+                        $k =0;
                         while($row = $results->fetch_row()){
+                            
+                            if($row[3] == 1){
+                                $primaryIndex = $i;
+                            }
+                            $columns[$row[0]] = $row[1];
+                            $columnsNamee[$k] = $row[0];
                             $i++;
+                            $k++;
                             ?>
                         <th><?php echo $row[0]; ?></th>
                         <?php } ?>
+                        <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
-                        <?php 
-                        
+                    <?php 
+                        // echo $primaryIndex;
                         $cnxS = new mysqli("localhost","root","",$_GET['db']);
                         $sql = "SELECT * FROM $tableName";
                         $results =  $cnxS->query($sql);
                         
                         while($row = $results->fetch_row()){?>
-                        <tr>    
-                            <?php 
+                    <tr>
+                        <?php 
                             for($j = 0 ; $j< $i;$j++){?>
-                                <td><?php echo $row[$j];?></td>
-                           <?php }
+                        <td><?php echo $row[$j];?></td>
+                        <?php }
                             ?>
-                        </tr>
-                           
-                           <?php } ?>
+                            <td><a href="Modifier.php<?php if($primaryIndex != -1) 
+                            {echo "?cle=".$row[$primaryIndex]; 
+                            echo "&idTable=".$_GET['id']."&db=".$_GET['db'] ; } 
+                            ?>" class="btn btn-success" id="Modifier" target="_blank">Modifier</a></td>
+                    </tr>
+
+                    <?php } ?>
                 </tbody>
             </table>
             <?php }  ?>
+            <?php 
+                if(isset($_POST['btnSub'])){
+                    $cnxS = new mysqli("localhost","root","",$_GET['db']);
+                    $sql = "INSERT INTO $tableName VALUES(";
+                    for ($i=0; $i < count($columnsNamee); $i++) { 
+                        if($columns[$columnsNamee[$i]] == "VARCHAR" || $columns[$columnsNamee[$i]] == "TEXT" ||  $columns[$columnsNamee[$i]] == "DATE"){
+                            if($i == 0){
+                                $sql .= "'".$_POST[$columnsNamee[$i]]."'";
+                            }  
+                            else{
+                                $sql .= ",'".$_POST[$columnsNamee[$i]]."'";
+                            } 
+                            
+                        }
+                       else{
+                            if($i == 0){
+                                $sql .= $_POST[$columnsNamee[$i]];
+                            }  
+                            else{
+                                $sql .= ",".$_POST[$columnsNamee[$i]];
+                            } 
+                            
+                        }
+                    }
+                        $sql .= ")";
+                        $results =  $cnxS->query($sql);
+                        if(!$results){
+                            echo 'il ya un probleme';
+                        }
+                        else{
+                            echo 'il ya pas un probleme';
+                        }
+                }
+            ?>
 
         </div>
     </div>
@@ -166,9 +272,13 @@
     <!-- <script src="js/popper.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/main.js"></script> -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
     <!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script> -->
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
     <script>
     $(document).ready(function() {
         $("#deleteDB").click(function() {
